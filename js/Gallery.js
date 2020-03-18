6ab8f60d66dcddcc7542550821e154ec62ea2b5f
Adam Markowski <markowski.adam93@gmail.com>
2020-03-18 12:43:33 +0100

    Run main function to handle arrow listeners

--- js/Gallery.js
+++ js/Gallery.js
@@ -12,19 +12,25 @@ const rightArrow = document.createElement('div');
 const exitBtn = document.createElement("div");
 
 leftArrow.classList.add("lightbox__left-arrow");
+leftArrow.textContent = "<";
+
 rightArrow.classList.add("lightbox__right-arrow");
+rightArrow.textContent = ">";
+
 exitBtn.classList.add("lightbox__exit-button");
+exitBtn.textContent = "x";
 
 
 const sectionsGallery = document.querySelectorAll(".section > .section__realization");
 
+let number = null;
+
 sectionsGallery.forEach(el => {
-    el.addEventListener("click", function (event) {
+    el.addEventListener("click", function () {
 
         index = this.dataset.key;
 
         lightbox.classList.add("active");
-
 
         img.classList.add("lightbox__container");
         img.style.backgroundImage = `url(/img/gallery-image/r${index}/1.jpg)`;
@@ -37,6 +43,8 @@ sectionsGallery.forEach(el => {
         img.appendChild(leftArrow);
         img.appendChild(rightArrow);
         img.appendChild(exitBtn);
+
+        number = 1;
     })
 })
 
@@ -44,9 +52,33 @@ exitBtn.addEventListener("click", () => {
     lightbox.classList.remove("active");
 })
 
-rightArrow.addEventListener("click", () => {
+const realizationArrayLenghts = [3, 5, 6, 6, 8, 4, 6, 7, 3, 5, 4, 4, 7, 6, 8];
 
-    let number = 2;
 
-    img.style.backgroundImage = `url(/img/gallery-image/r${index}/${number}.jpg)`;
-})
+function moveToNextImg(num, direction) {
+
+    if (direction) {
+        if (number === realizationArrayLenghts[num - 1]) {
+            number = 0;
+        }
+
+        number += 1;
+
+    } else if (!direction) {
+        if (number === 1) {
+            number = realizationArrayLenghts[num - 1] + 1;
+        }
+
+        number -= 1;
+    }
+
+    img.style.backgroundImage = `url(/img/gallery-image/r${num}/${number}.jpg)`
+}
+
+rightArrow.addEventListener("click", function () {
+    moveToNextImg(index, true);
+});
+
+leftArrow.addEventListener("click", function () {
+    moveToNextImg(index, false);
+});
